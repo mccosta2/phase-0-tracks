71156c306f8a29e0a5fb0998b9b79bfb36fad705
Michael Costa <mccosta2@gmail.com>
2017-02-13 22:39:55 -0600

    added catch all to say never mind

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,53 +1,57 @@
 def vampdetector()
-	puts "What is your name?"
-	name = gets.chomp
+	def vampdetectorinner()
+		puts "What is your name?"
+		name = gets.chomp
 
-	puts "How old are you?"
-	age = gets.chomp
-	age = age.to_i
+		puts "How old are you?"
+		age = gets.chomp
+		age = age.to_i
 
-	puts "What year were you born?"
-	year = gets.chomp
-	year = year.to_i
+		puts "What year were you born?"
+		year = gets.chomp
+		year = year.to_i
 
-	puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-	garlic = gets.chomp
+		puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+		garlic = gets.chomp
 
-	puts "Would you like to enroll in the company’s health insurance?"
-	insurance = gets.chomp
+		puts "Would you like to enroll in the company’s health insurance?"
+		insurance = gets.chomp
 
-	allergies = ""
-	until allergies == "done" do
-		puts "you have any allergies?"
-		allergies = gets.chomp
-		if allergies == "sunshine"
-			puts "Probably a vampire"
-			return
+		allergies = ""
+		until allergies == "done" do
+			puts "you have any allergies?"
+			allergies = gets.chomp
+			if allergies == "sunshine"
+				puts "Probably a vampire"
+				return
+			end
 		end
-	end
 
-	agecalc = 2017-year
-	agecalc2 = 2017-year-1
+		agecalc = 2017-year
+		agecalc2 = 2017-year-1
 
-	case name
-	when "Drake Cula"
-		puts "Definitely a vampire."
-	when "Tu Fang"
-		puts "Definitely a vampire."
-	else
-		if (age == agecalc)||(age == agecalc2)
-			if (garlic == "y") || (insurance == "y")
-				puts "Probably not a vampire."
-			end
-			if (garlic == "n") && (insurance == "n")
-				puts "Almost certainly a vampire."
-			else 
-				puts "Probably a vampire."
-			end
+		case name
+		when "Drake Cula"
+			puts "Definitely a vampire."
+		when "Tu Fang"
+			puts "Definitely a vampire."
 		else
-			puts "Results inconclusive"
+			if (age == agecalc)||(age == agecalc2)
+				if (garlic == "y") || (insurance == "y")
+					puts "Probably not a vampire."
+				end
+				if (garlic == "n") && (insurance == "n")
+					puts "Almost certainly a vampire."
+				else 
+					puts "Probably a vampire."
+				end
+			else
+				puts "Results inconclusive"
+			end
 		end
 	end
+	vampdetectorinner()
+	puts  "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
 end
 
 puts "How many employees are to be processed?"
