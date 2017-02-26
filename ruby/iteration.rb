a53fdbf913f3ea66a613be85b3ef1987564af7e7
Michael Costa <mccosta2@gmail.com>
2017-02-26 11:09:19 -0600

    added an array and hash, played with each and map

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -4,3 +4,40 @@ def talker
 end
 
 talker { |your_name| puts "hi #{your_name}, blah blah blah, after block!"}
+
+dog_types = [
+"German Shepard",
+"Dalmation",
+"Husky",
+"Hot"
+]
+
+fruit_qualities = {
+	"banana" => "long",
+	"orange" => "round",
+	"lemon" => "sour",
+	"dragonfruit" => "mysterious"
+}	
+
+dog_types.each do |breed|
+	puts "My favorite type of dog is #{breed}"
+end
+
+fruit_qualities.each do |fruit, description|
+	puts "a #{fruit} is #{description}"
+end
+
+puts dog_types
+
+dog_types.map do |dog|
+	dog.upcase
+end
+
+puts dog_types
+
+dog_types.map! do |dog|
+	dog.upcase
+end
+
+puts dog_types
+
