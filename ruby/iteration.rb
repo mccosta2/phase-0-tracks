2b54b75fdd7afcf203e531d1e06ea50703480806
Michael Costa <mccosta2@gmail.com>
2017-02-26 12:18:23 -0600

    complete release 2 leveraging documentation

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -41,3 +41,100 @@ end
 
 puts dog_types
 
+#A method that iterates through the items, deleting any that meet a certain condition (for example, deleting any numbers that are less than 5).
+samp_arr =  [
+"a",
+"b",
+"c",
+"d"
+]
+
+samp_hash = {
+	1 => "one",
+	2 => "two",
+	3 => "three",
+	4 => "four"
+}	
+
+p samp_arr
+p samp_hash
+
+samp_arr.delete_if  {|item| item <= "b"}
+samp_hash.delete_if {|digit, word| digit < 3 }
+
+p samp_arr
+p samp_hash
+
+
+#A method that filters a data structure for only items that do satisfy a certain condition (for example, keeping any numbers that are less than 5).
+	
+samp_arr =  [
+"a",
+"b",
+"c",
+"d"
+]
+
+samp_hash = {
+	1 => "one",
+	2 => "two",
+	3 => "three",
+	4 => "four"
+}	
+
+p samp_arr
+p samp_hash
+
+p samp_arr.keep_if {|item| item <= "b"}
+samp_hash.keep_if {|digit, word| digit < 3 }
+
+p samp_hash
+
+
+#A different method that filters a data structure for only items satisfying a certain condition -- Ruby offers several options!	
+
+samp_arr =  [
+"a",
+"b",
+"c",
+"d"
+]
+
+samp_hash = {
+	1 => "one",
+	2 => "two",
+	3 => "three",
+	4 => "four"
+}	
+
+p samp_arr
+p samp_hash
+
+p samp_arr.take_while {|item| item <= "b"}
+samp_hash.reject! {|digit, word| not (digit < 3) }
+
+p samp_hash
+
+#A method that will remove items from a data structure until the condition in the block evaluates to false, then stops (you may not find a perfectly working option for the hash, and that's okay).
+samp_arr =  [
+"a",
+"b",
+"c",
+"d"
+]
+
+samp_hash = {
+	1 => "one",
+	2 => "two",
+	3 => "three",
+	4 => "four"
+}	
+
+p samp_arr
+p samp_hash
+
+p samp_arr.drop_while  {|item| item <= "c"}
+samp_hash.reject! {|digit, word|  (digit < 3) }
+
+
+p samp_hash
