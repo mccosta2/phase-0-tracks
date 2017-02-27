bf9ad78d2aa8fb23ebbdb6cff0a8abee656601a8
Michael Costa <mccosta2@gmail.com>
2017-02-27 13:37:47 -0600

    do the thing for 6.2

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,32 @@ class Puppy
     toy
   end
 
+  def speak(int)
+    int.times  do puts "Woof!"
+    end
+  end
+
+  def roll_over()
+    puts "*rolls over*"
+  end
+
+  def dog_years(int)
+    dy = int*7  
+    puts dy
+    dy
+  end
+
+  def play_dead()
+ 	puts "..."
+ end
+
 end
+
+shepherd = Puppy.new
+shepherd.fetch("ball")	
+shepherd.speak(4)
+shepherd.roll_over
+shepherd.dog_years(5)
+shepherd.play_dead
+
 
