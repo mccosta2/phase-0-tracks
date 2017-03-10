88e9871593c495c59fc629df43419f72f0e9aca7
Michael Costa <mccosta2@gmail.com>
2017-03-10 11:17:43 -0600

    created reverse function

---
+++ js/explore.js
@@ -0,0 +1,20 @@
+//pass a string to function
+//cycle through the letters in reverse
+//print letter 
+
+var str = "hello";
+
+
+function reverse(str) {
+	var	str2 ="";
+	for (var i = 1; i < str.length+1; i++) {
+//		console.log(str[str.length-i]);
+		str2 += str[str.length-i];
+
+		}
+	return str2;
+	}
+
+if (1 == 1) {
+console.log(reverse("monkey"));
+}
