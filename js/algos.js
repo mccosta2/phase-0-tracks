0fe2cf908a23640a3988ec5aa2332a29203d8a85
Michael Costa <mccosta2@gmail.com>
2017-03-10 14:26:38 -0600

    completed final release

--- js/algos.js
+++ js/algos.js
@@ -24,7 +24,52 @@ function longest(array) {
 }
 
 
-//driver
+//create a function to compare key value pairs in two objects
+//create function that takes in two object paramters
+function comparer(obj1, obj2) {
+//run through first object and compare to second
+	for (var key in obj1) {
+		if (obj1[key] == obj2[key]) {
+			return true;
+		}
+	}
+	for (var key in obj2) {
+		if (obj2[key] == obj1[key]) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+//run through second object and compare to first
+}
+
+
+//function for random arrays
+function wordgen(int) {
+	var word_array = []
+	for (var i = 0; i < int; i++) {
+		var random_num = Math.floor((Math.random()*10)+1);
+		var word_base = "asdfghjklz";
+		var word = "";
+		for (var j = 0; j < random_num; j++) {
+			word += word_base[j];
+		}
+		word_array.push(word);
+	}
+	console.log(word_array)
+	return word_array;
+}
+//create function that takes in int
+//create loop int times
+//each time add a word that is 1 to 10 letters long
+//create random number 1 to 10
+//to array add word that is based off
+//the word will be based off "asdfghjklz"
+
+
+
+//driver for longest
 // create sample array variable
 var wordlist = ["long phrase","longest phrase","longer phrase"];
 console.log(longest(wordlist));
@@ -35,3 +80,14 @@ var wordlist3 = ["one", "seventeen", "twenty"]
 console.log(longest(wordlist2));
 console.log(longest(wordlist3));
 
+//driver for comparer
+//should return true
+console.log(comparer({name: "Steven", age: 54}, {name: "Tamir", age: 54}));
+//should return false
+console.log(comparer({name: "Steven", age: 54}, {dog: "Lola"}));
+
+
+//driver for wordgen
+for (var i = 0; i < 10; i++) {
+console.log(longest(wordgen(4)));
+}
