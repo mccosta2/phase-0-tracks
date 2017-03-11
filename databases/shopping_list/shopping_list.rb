67094d06cbbd4a3368b8bc3854d4a41cac04fcee
Michael Costa <mccosta2@gmail.com>
2017-03-11 14:07:47 -0600

    testing basic driver logic

--- databases/shopping_list/shopping_list.rb
+++ databases/shopping_list/shopping_list.rb
@@ -28,9 +28,44 @@ db.execute(create_table_cmd)
 
 # Can users create data? Retrieve it? Manipulate it?
 
-def adding_item(item, quantity = 1, store = "Costco")
+def adding_item(item, quantity, store)
 
   db.execute("INSERT INTO groceries (item, quantity, store) VALUES (?, ?, ?)", [item, quantity, store])
+
+end
+
+def delete(item)
+
+  db.execute("DELETE FROM groceries where item = ?", [item])
+
+end
+
+def update(item, quantity)
+
+  db.execute("UPDATE groceries set quantity = ? where item = ?", [quantity, item])
+
+end
+#handle string parsing later
+def add_parse(splicedarray)
+	#defaults
+	quantity = 1
+	store = 'Costco'
+	item = ''
+	#determine if there is a quanity, if so remove it and move on
+	if splicedarray.first.to_i != 0
+		quantity = splicedarray.first.to_i
+		splicedarray.drop(1)
+	end
+	#determine if there is a store then remove
+	if splicedarray.index('from') != nil
+		index = splicedarray.index('from')
+		store = splicedarray[index+1..-1].join(' ')
+		item =  splicedarray[1..index-1].join(' ')
+	else
+		item = splicedarray.join(' ')	
+	end
+
+adding_item(item, quantity, store)
 
 end
 
@@ -43,32 +78,40 @@ action = gets.chomp
 
 while action != 'done' do
 	if action == 'add'
-		puts "what would you like to add? (quantity <optional>, item (no spaces), 'from' store <optional>)"
-		puts "ex: '12 bananas' or '2%_milk from costco' or '2 organic_bell_peppers from Marianos'":
-		new_item = gets.chomp.slice('')
-		#do some stuff here to figure out what new item is 
-
+		puts "what would you like to add? (quantity <optional>, item, 'from' store <optional>)"
+		puts "ex: '12 bananas' or 'organic milk from costco' or '2 green bell peppers from Marianos'":
+		new_item = gets.chomp.slice(' ')
+		add_parse(new_item)
+		
 		#do some stuff to add item 		adding_item
 	elsif action == view
-		puts "what would you like to add? (quantity <optional>, item (no spaces), 'from' store <optional>)"
-		puts "ex: '12 bananas' or '2%_milk from costco' or '2 organic_bell_peppers from Marianos'":
-		new_item = gets.chomp.slice('')
+		#for now just test then clean
+		groceries = db.execute("SELECT * FROM groceries")
+		puts groceries
 	elsif action == 'update'
 		puts "what item would you like to change? ..."
 		#add example items from actual list above
 		update_item = gets.chomp
-		
+		puts "either type 'delete' or enter new quantity"
+		#maybe later add ability to change store
+		update_action = gets.chomp
+		if update_action == 'delete'
+			delete(update_item)
+		else
+			update(update_item, update_action)
+		end
 		new_item = gets.chomp.slice('')
 	else 
 		break
+
 	end
-		
+	
+	Puts "Please let me know what you'd like to do next: 'add', 'view', 'update' or 'done':"
+	action = gets.chomp	
 
 end
 
 
-groceries = db.execute("SELECT * FROM groceries")
-# kittens.each do |kitten|
-puts groceries
-# end
+# groceries = db.execute("SELECT * FROM groceries")
+# puts groceries
 
